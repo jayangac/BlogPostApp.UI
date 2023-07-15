b16fcba5e927df2dde93d169658eefa19a4b9301
jayanga <jay.chandrakumara@hotmail.com>
2023-07-15 19:33:37 +0800

    adding category componets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './core/components/navbar/navbar/navbar.component';
+import { NavbarComponent } from './core/components/navbar/navbar.component';
+import { AddCategoryComponent } from './features/category/add-category/add-category.component';
+import { CategoryListComponent } from './features/category/category-list/category-list.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    AddCategoryComponent,
+    CategoryListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
